bbf859c9db38e5d2191acec0f03d84a16866a5ba
gonexwind <fikky.ardi15@gmail.com>
2021-07-22 22:24:47 +0700

    recycler list

--- app/src/main/java/com/fikkyardianto/herokotlin/MainActivity.kt
+++ app/src/main/java/com/fikkyardianto/herokotlin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.fikkyardianto.herokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
